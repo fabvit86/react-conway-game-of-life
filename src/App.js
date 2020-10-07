750783d6c19b0ddefd21a0301081280883202755
Fabrizio Vitelli <fabvit86@yahoo.it>
2020-10-08 00:08:07 +0200

    migrated project from codepen

--- src/App.js
+++ src/App.js
@@ -1,11 +1,71 @@
-import React from 'react';
-import './App.css';
+//RULES:
+/*
+Any live cell with fewer than two live neighbours dies, as if caused by underpopulation.
+Any live cell with two or three live neighbours lives on to the next generation.
+Any live cell with more than three live neighbours dies, as if by overpopulation.
+Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+*/
 
-function App() {
-  return (
-    <div>
-    </div>
-  )
+import React, { Component } from 'react'
+import './App.css';
+import Grid from './Grid'
+
+class App extends Component {
+  constructor () {
+    super()
+    this.state = {
+      gameId: 1
+    }
+    // default values:
+    this.rows = 40
+    this.columns = 70
+    this.activeGridButton = 'mediumGridButton'
+  }
+  
+  // change the gameId (key of <Grid/>) to render a new game:
+  newGame () {
+    this.setState({ gameId: this.state.gameId + 1 })
+  }
+  
+  //change the game grid size and start a new game:
+  changeGridSize (sizeButtonId) {
+    switch (sizeButtonId) {
+      case 'smallGridButton':
+        this.rows = 20
+        this.columns = 50    
+      break
+      case 'mediumGridButton':
+        this.rows = 40
+        this.columns = 70    
+      break
+      case 'largeGridButton':
+        this.rows = 60
+        this.columns = 90    
+      break
+      default:
+        break
+    }
+    this.activeGridButton = sizeButtonId
+    this.newGame()
+  }
+  
+  render () {
+    return (
+      <div>
+        <Grid
+          key={this.state.gameId}
+          newGame={this.newGame.bind(this)}
+          changeGridSize={this.changeGridSize.bind(this)}
+          rows={this.rows}
+          columns={this.columns}
+          activeGridButton={this.activeGridButton}
+          slowSpeed={700}
+          mediumSpeed={400}
+          fastSpeed={100}
+        />
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ src/ControlButton.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import $ from 'jquery'
+
+// stateless, functional component rappresenting a button:
+const ControlButton = ({id, classes, onClickHandler, name, action, gameState}) => {
+  const handleClick = () => {
+    let buttonsToDisactivate = ''
+    switch (action) {
+      case 'play':
+        if (gameState !== 'play') {
+          onClickHandler('play')
+          buttonsToDisactivate = 'gameButton'
+        }
+      break
+      case 'pause':
+        if (gameState === 'play') {
+          onClickHandler('pause')
+          buttonsToDisactivate = 'gameButton'
+        }
+      break
+      case 'clear':
+        onClickHandler('clear')
+        buttonsToDisactivate = 'gameButton'
+      break
+      case 'changeGrid':
+        onClickHandler(id)
+        buttonsToDisactivate = 'sizeButton'
+      break
+      case 'changeSpeed':
+        onClickHandler(id)
+        buttonsToDisactivate = 'speedButton'
+      break
+      default:
+      break
+    }
+    $('.'+buttonsToDisactivate).removeClass('active')
+    $('#'+id).addClass('active')
+  }
+
+  return (
+    <button 
+      id={id} 
+      className={classes} 
+      onClick={action === undefined ? onClickHandler : handleClick}>
+      {name}
+    </button>
+  )
+
+  
+} // ControlButton component
+
+export default ControlButton
